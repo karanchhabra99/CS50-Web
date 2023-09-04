7158fbad3552c2fdee81a5d2b9d8f927184af35a
Karan Chhabra <karanchhabra99@gmail.com>
2023-09-04 15:00:04 -0700

    create, edit, random page and markdown conversion

--- project-1/wiki/encyclopedia/views.py
+++ project-1/wiki/encyclopedia/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404,HttpResponse
+from django import forms
+import random
+import markdown2
+
+  
 
 from . import util
+
+class Wiki_Form(forms.Form):
+    title = forms.CharField(label="Title")
+    body = forms.CharField(label="Body", widget=forms.Textarea(attrs={'style': 'height: 40em;'}))
 
 
 
@@ -9,7 +18,7 @@ def wikipage(request, wikiname):
     '''
     This helps in rendering wikipages when clicked on hyperlink, searched
     '''
-    data =util.get_entry(wikiname)
+    data =markdown2.markdown(util.get_entry(wikiname))
     if data != None:
         return render(request, "encyclopedia/wikiname.html", {
         "wikiname": wikiname.capitalize(), "data": data
@@ -33,4 +42,44 @@ def index(request):
     })
 
 def wikiname(request, wikiname):
+    if wikiname == 'Select a Random entry':
+        wikiname = random.choice(util.list_entries())
     return wikipage(request, wikiname)
+
+
+def create_wiki(request):
+    if request.method == "POST":
+        form = Wiki_Form(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            if title in util.list_entries():
+                return render(request, "encyclopedia/Create_Error.html", {
+                "entries": util.list_entries()
+            })
+            util.save_entry(title, body)
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
+            })
+
+    return render(request, "encyclopedia/create_wiki.html", {
+        "form": Wiki_Form()
+    })
+
+
+def edit_wiki(request, wikiname="POST"):
+    if request.method == "POST":
+        form = Wiki_Form(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            util.save_entry(title, body)
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
+            })
+        
+    return render(request, "encyclopedia/edit_wiki.html", {
+        "form": Wiki_Form({"title": wikiname, "body":util.get_entry(wikiname)})
+    })
